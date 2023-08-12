d2c020f415f59cd550e9f7883a917bb74a2cfdd4
teddynalle <51263105+teddynalle@users.noreply.github.com>
2023-08-12 16:26:03 +0200

    Update skeppet.js

--- skeppet.js
+++ skeppet.js
@@ -2,8 +2,8 @@ let btn1 = document.querySelector('#btn-1');
 let btn2 = document.querySelector('#btn-2');
 
 btn1.addEventListener('click', () =>{
-  document.body.style.backgroundImage ="url('/jpg/akter.jpg')"
+  document.body.style.backgroundImage ="url('jpg/akter.jpg')"
 });
 btn2.addEventListener('click', () =>{
-  document.body.style.backgroundImage ="url('/jpg/skeppet1.jpg')"
+  document.body.style.backgroundImage ="url('jpg/skeppet1.jpg')"
 });
